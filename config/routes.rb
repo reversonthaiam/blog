ba71027e433e606c5aa406d8d6bb0709101de943
Reverson <reverson.santos@irancho.com.br>
2024-12-03 12:06:04 -0300

    new styles

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "inicio", controller: "home", action: "index", as: "home"
 
   get "students", controller: "students", action: "index", as: "estudantes"
+  
+  root "home#index"
 end
